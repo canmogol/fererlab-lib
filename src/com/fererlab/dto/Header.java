5d58af3f7276dc05af199f51fd4e3010b80abf40
Can A. MOGOL <canmogol@gmail.com>
2014-02-23 22:06:03 +0200

    message key set

--- src/com/fererlab/dto/Header.java
+++ src/com/fererlab/dto/Header.java
@@ -13,7 +13,7 @@ public class Header<Key, Value> extends Pair<Key, Value> {
     public static final String STATUS = "status";
     public static final String STATUS_SUCCESS = "success";
     public static final String STATUS_FAIL = "fail";
-    public static final String MESSAGE = "";
+    public static final String MESSAGE = "message";
 
     public Header<Key, Value> add(Key key, Value value) {
         return (Header<Key, Value>) super.add(key, value);
